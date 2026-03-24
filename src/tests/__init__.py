55fcb5596eaefa8db582475bf8d429ec08c3ac33
allixx <1695323+allixx@users.noreply.github.com>
2026-03-24 20:33:25 +0300

    Fix ty diagnostics
    With gradual introduction of type hints to codebase, a choice of
    typechecker is due. I suggest Astral's `ty` as it is very fast, and
    actively developed.

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,11 +1,17 @@
 """Hack to import foomuuri without .py extension and proper package tree."""
 import sys
+from importlib.abc import Loader
 from importlib.machinery import SourceFileLoader
 from importlib.util import module_from_spec, spec_from_loader
 
-spec = spec_from_loader(
-    "foomuuri", SourceFileLoader("foomuuri", "./src/foomuuri")
-)
-foomuuri = module_from_spec(spec)
-spec.loader.exec_module(foomuuri)
-sys.modules['foomuuri'] = foomuuri
+if (
+    (spec := spec_from_loader(
+        'foomuuri', SourceFileLoader('foomuuri', './src/foomuuri')
+    ))
+    and (foomuuri := module_from_spec(spec))
+    and isinstance(spec.loader, Loader)
+):
+    spec.loader.exec_module(foomuuri)
+    sys.modules['foomuuri'] = foomuuri
+else:
+    raise SystemError("Failed to add foomuuri to sys.modules")
