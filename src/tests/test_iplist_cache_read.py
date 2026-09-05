16a461b824652bbe2499faecc6e25acbc340c0d5
allixx <1695323+allixx@users.noreply.github.com>
2026-09-05 11:21:29 +0300

    Don't cache transient iplist addresses
    No longer stores iplist addresses collected from its sources in cache
    file.
    
    `IPListCache` becomes `IPListSourceCache`.

--- src/tests/test_iplist_cache_read.py
+++ src/tests/test_iplist_cache_read.py
@@ -1,4 +1,4 @@
-"""Test IPListCache.read()."""
+"""Test IPListSourceCache.read()."""
 
 # pylint: disable=invalid-name,import-error
 
@@ -8,21 +8,25 @@ import unittest.mock
 import foomuuri
 
 
-class TestIPListCacheRead(unittest.TestCase):
-    """Test IPListCache.read()."""
+class TestIPListSourceCacheRead(unittest.TestCase):
+    """Test IPListSourceCache.read()."""
 
     def test_ignore_removed_entries(self):
-        """Test ignore unknown cache entries."""
+        """Test ignore unknown and legacy transient cache entries."""
         data = {
             '@unknown': {
-                'ip': {'10.0.0.1': foomuuri.IPListCache.expire_forever},
+                'ip': {'10.0.0.1': foomuuri.IPListSourceCache.expire_forever},
             },
             'manual.@unknown': {
-                'ip': {'10.0.0.2': foomuuri.IPListCache.expire_forever},
+                'ip': {'10.0.0.2': foomuuri.IPListSourceCache.expire_forever},
+            },
+            'manual.@known': {
+                'ip': {'10.0.0.4': foomuuri.IPListSourceCache.expire_forever},
+                'refresh': 1,
             },
             'broken': {'dirty': False},
             '@known': {
-                'ip': {'10.0.0.3': foomuuri.IPListCache.expire_forever},
+                'ip': {'10.0.0.3': foomuuri.IPListSourceCache.expire_forever},
                 'refresh': 1,
             },
         }
@@ -33,7 +37,7 @@ class TestIPListCacheRead(unittest.TestCase):
             unittest.mock.patch('foomuuri.state_file', return_value=filename),
             unittest.mock.patch('foomuuri.verbose'),
         ):
-            cache = foomuuri.IPListCache.read({'@known'})
+            cache = foomuuri.IPListSourceCache.read({'@known'})
 
-        self.assertEqual(set(cache), {'@known'})
-        self.assertEqual(cache['@known'], data['@known'])
+        self.assertEqual(set(cache), {'manual.@known'})
+        self.assertEqual(cache['manual.@known'], data['manual.@known'])
